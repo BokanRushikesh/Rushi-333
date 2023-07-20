b65ff556b0b4876a59d4ac4e9fc529656def27d8
BokanRushikesh <rushikeshbokan07@gmail.com>
2023-07-20 14:35:10 +0530

    [Rushikesh Bokan] - Add new Class

---
+++ src/main/java/baseclasses/LaunchCode.java
@@ -0,0 +1,20 @@
+package baseclasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchCode {
+
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://en-gb.facebook.com/reg/");
+		
+		driver.manage().window().maximize();
+		
+		driver.close();
+	}
+}
+// ""
